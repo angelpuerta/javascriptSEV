b9996d78645b29406aaaa443de77847298caafdc
Angel <angelpuertadiaz@gmail.com>
2018-11-06 19:12:05 +0100

    Primer commit

---
+++ src/EventosTeclado.js
@@ -0,0 +1,88 @@
+var teclas = [];
+
+window.addEventListener('keydown', onKeyDown, false);
+window.addEventListener('keyup', onKeyUp, false);
+
+function onKeyDown(event) {
+    entrada = entradas.teclado;
+    // agregar la tecla pulsada si no estaba
+    var posicion = teclas.indexOf(event.keyCode);
+    if (posicion == -1) {
+        teclas.push(event.keyCode);
+        switch (event.keyCode) {
+            case 32:
+                controles.continuar = true;
+                break;
+            case 27:
+                controles.pausar = true;
+                break;
+            case TECLAS.ATAQUE_ARRIBA:
+                controles.disparo = orientaciones.arriba;
+                break;
+            case TECLAS.ATAQUE_ABAJO:
+                controles.disparo = orientaciones.abajo;
+                break;
+            case TECLAS.ATAQUE_DERECHA:
+                controles.disparo = orientaciones.derecha;
+                break;
+            case TECLAS.ATAQUE_IZQUIERDA:
+                controles.disparo = orientaciones.izquierda;
+                break;
+            case TECLAS.ABAJO:
+                controles.moverY = 1;
+                break;
+            case TECLAS.ARRIBA:
+                controles.moverY = -1;
+                break;
+            case TECLAS.DERECHA:
+                controles.moverX = 1;
+                break;
+            case TECLAS.IZQUIERDA:
+                controles.moverX = -1;
+                break;
+        }
+    }
+}
+
+function onKeyUp(event) {
+    // sacar la tecla pulsada
+    var posicion = teclas.indexOf(event.keyCode);
+    teclas.splice(posicion, 1);
+    console.log("Tecla levantada");
+
+    switch (event.keyCode) {
+        case 32:
+            controles.continuar = false;
+            break;
+        case 27:
+            controles.pausar = false;
+            break;
+        case TECLAS.ABAJO:
+            if (controles.moverY == 1) {
+                controles.moverY = 0;
+            }
+            break;
+        case TECLAS.ARRIBA:
+            if (controles.moverY == -1) {
+                controles.moverY = 0;
+            }
+            break;
+        case TECLAS.DERECHA:
+            if (controles.moverX == 1) {
+                controles.moverX = 0;
+            }
+            break;
+        case TECLAS.IZQUIERDA:
+            if (controles.moverX == -1) {
+                controles.moverX = 0;
+            }
+            break;
+        case TECLAS.ATAQUE_ARRIBA:
+        case TECLAS.ATAQUE_ABAJO:
+        case TECLAS.ATAQUE_DERECHA:
+        case TECLAS.ATAQUE_IZQUIERDA:
+            controles.disparo = 0;
+            break;
+    }
+}
+
---
+++ src/Globales.js
@@ -0,0 +1,45 @@
+var pulsaciones = []; // actuales registradas
+
+var tipoPulsacion = {}; // tipos
+tipoPulsacion.inicio = 1;
+tipoPulsacion.mantener = 2;
+
+var entradas = {}; // tipos
+entradas.pulsaciones = 1;
+entradas.teclado = 2;
+entradas.gamepad = 3;
+var entrada = entradas.pulsaciones;
+
+
+var nivelActual = 0;
+var nivelMaximo = 2;
+
+var cuerpo = {};
+cuerpo.dinamico = 1;
+cuerpo.estatico = 2;
+
+var estados = {};
+estados.moviendo= 2; // Incluye parado, derecha , izquierda
+estados.saltando = 3;
+estados.muriendo = 4;
+estados.muerto = 5;
+estados.disparando = 6;
+estados.impactado = 7;
+
+var orientaciones = {};
+orientaciones.arriba = 1;
+orientaciones.abajo = 2;
+orientaciones.derecha = 3;
+orientaciones.izquierda = 4;
+
+var TECLAS = {};
+TECLAS.ARRIBA = 87;
+TECLAS.ABAJO = 83;
+TECLAS.DERECHA = 68;
+TECLAS.IZQUIERDA = 65;
+TECLAS.BOMBA = 81;
+TECLAS.RECURSO = 32;
+TECLAS.ATAQUE_ARRIBA = 38;
+TECLAS.ATAQUE_ABAJO = 40;
+TECLAS.ATAQUE_DERECHA = 39;
+TECLAS.ATAQUE_IZQUIERDA = 37;
