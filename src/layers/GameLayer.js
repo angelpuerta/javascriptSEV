63c82c3974eff9f6aebdd31b4602ae268d24d075
carlosmm96 <carlosmm96@users.noreply.github.com>
2018-11-29 20:26:48 +0100

    Update GameLayer.js

--- src/layers/GameLayer.js
+++ src/layers/GameLayer.js
@@ -151,6 +151,8 @@ class GameLayer extends Layer {
         for (var i = 0; i < this.disparosEnemigo.length; i++) {
             if (this.disparosEnemigo[i] != null && this.disparosEnemigo[i].colisiona(this.jugador)) {
                 this.jugador.golpeado();
+                reproducirEfecto(efectos.golpeo);
+        
                 this.disparosEnemigo.splice(i, 1);
             }
         }
@@ -230,6 +232,7 @@ class GameLayer extends Layer {
         for (var i = 0; i < this.explosiones.length; i++) {
             if (this.jugador.colisiona(this.explosiones[i])) {
                 this.jugador.golpeado();
+                reproducirEfecto(efectos.golpeo);
 
             }
             for (var j = 0; j < this.enemigos.length; j++) {
@@ -251,7 +254,7 @@ class GameLayer extends Layer {
                 if (this.disparosJugador[i] != null &&
                     this.enemigos[j] != null &&
                     this.disparosJugador[i].colisiona(this.enemigos[j])) {
-
+                    reproducirEfecto(efectos.da);
                     this.espacio
                         .eliminarCuerpoDinamico(this.disparosJugador[i]);
                     this.enemigos[j].impactado(this.disparosJugador[i]);
@@ -307,6 +310,7 @@ class GameLayer extends Layer {
 
         if (this.jugador.vidas <= 0) {
             this.iniciar();
+            reproducirEfecto(efectos.muere;
         }
 
     }
